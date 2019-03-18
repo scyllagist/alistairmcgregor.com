92776b8d3a5406f9f49c7e2f8df0b24108f4e2e1
Alistair William McGregor <alistair.mcgregor@gmail.com>
2019-03-17 17:47:57 -0700

    Naively trying to add animation with scrolling

--- js/index.js
+++ js/index.js
@@ -40,6 +40,7 @@ var svg = d3.select("body").append("svg")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 d3.select("body").on("ontouchstart" in document ? "touchmove" : "mousemove", move);
+d3.select("body").on("scroll", move);
 
 d3.select(window).on('resize', resize); 
 
