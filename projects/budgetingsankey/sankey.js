5a04cbfc1076a5abea5b1830e453813738595d45
Alistair William McGregor <alistair.mcgregor@gmail.com>
2019-05-18 17:48:11 -0700

    Create sankey.js

---
+++ projects/budgetingsankey/sankey.js
@@ -0,0 +1,258 @@
+var alistairData = 
+{'nodes': [{'node': 0, 'name': 'indulgence'},
+  {'node': 1, 'name': 'living expenses'},
+  {'node': 2, 'name': 'travel'},
+  {'node': 3, 'name': 'alcohol'},
+  {'node': 4, 'name': 'concerts'},
+  {'node': 5, 'name': 'other indulgences'},
+  {'node': 6, 'name': 'vaping'},
+  {'node': 7, 'name': 'Memberships / fees'},
+  {'node': 8, 'name': 'food'},
+  {'node': 9, 'name': 'groceries'},
+  {'node': 10, 'name': 'insurance'},
+  {'node': 11, 'name': 'medical'},
+  {'node': 12, 'name': 'misc living expenses'},
+  {'node': 13, 'name': 'rent'},
+  {'node': 14, 'name': 'driving'},
+  {'node': 15, 'name': 'flying'},
+  {'node': 16, 'name': 'other travel'},
+  {'node': 17, 'name': 'Internet'},
+  {'node': 18, 'name': 'Turbotax'},
+  {'node': 19, 'name': 'amazon'},
+  {'node': 20, 'name': 'checkbook'},
+  {'node': 21, 'name': 'credit card purchase interest charge'},
+  {'node': 22, 'name': 'misc memberships'},
+  {'node': 23, 'name': 'Online / Subscription'},
+  {'node': 24, 'name': 'gas station'},
+  {'node': 25, 'name': 'liquor store'},
+  {'node': 26, 'name': 'total wine'},
+  {'node': 27, 'name': 'Festival'},
+  {'node': 28, 'name': 'Venue'},
+  {'node': 29, 'name': 'Service / Tow'},
+  {'node': 30, 'name': 'gas'},
+  {'node': 31, 'name': 'parking'},
+  {'node': 32, 'name': 'rideshare'},
+  {'node': 33, 'name': 'uhaul'},
+  {'node': 34, 'name': 'united'},
+  {'node': 35, 'name': 'Dine-in'},
+  {'node': 36, 'name': 'delivery'},
+  {'node': 37, 'name': 'Food City'},
+  {'node': 38, 'name': 'Fry’s'},
+  {'node': 39, 'name': 'Target'},
+  {'node': 40, 'name': 'Trader Joe’s'},
+  {'node': 41, 'name': 'Web / delivery'},
+  {'node': 42, 'name': 'other'},
+  {'node': 43, 'name': 'other stores'},
+  {'node': 44, 'name': 'lemonade'},
+  {'node': 45, 'name': 'Dentist / teeth'},
+  {'node': 46, 'name': 'psychiatrist'},
+  {'node': 47, 'name': 'supplements'},
+  {'node': 48, 'name': 'Amazon purchases'},
+  {'node': 49, 'name': 'IKEA'},
+  {'node': 50, 'name': 'Mail and postage'},
+  {'node': 51, 'name': 'Mr. Sashimi'},
+  {'node': 52, 'name': 'Web subscriptions / hardware'},
+  {'node': 53, 'name': 'haircut'},
+  {'node': 54, 'name': 'laundry'},
+  {'node': 55, 'name': 'moving help'},
+  {'node': 56, 'name': 'gifts'},
+  {'node': 57, 'name': 'hobbies'},
+  {'node': 58, 'name': 'movies'},
+  {'node': 59, 'name': 'music'},
+  {'node': 60, 'name': 'parks and museums'},
+  {'node': 61, 'name': 'video games'},
+  {'node': 62, 'name': 'biking'},
+  {'node': 63, 'name': 'hotels'},
+  {'node': 64, 'name': 'public transportation'},
+  {'node': 65, 'name': 'scooter'},
+  {'node': 66, 'name': 'brick and mortar'},
+  {'node': 67, 'name': 'online'}],
+ 'links': [{'source': 0, 'target': 3, 'value': 5051.479999999991},
+  {'source': 0, 'target': 4, 'value': 1073.35},
+  {'source': 0, 'target': 5, 'value': 1839.1299999999999},
+  {'source': 0, 'target': 6, 'value': 1347.06},
+  {'source': 1, 'target': 7, 'value': 763.6400000000001},
+  {'source': 1, 'target': 8, 'value': 3816.37},
+  {'source': 1, 'target': 9, 'value': 4864.039999999998},
+  {'source': 1, 'target': 10, 'value': 109.36},
+  {'source': 1, 'target': 11, 'value': 215.95},
+  {'source': 1, 'target': 12, 'value': 3159.189999999999},
+  {'source': 1, 'target': 13, 'value': 8589.31},
+  {'source': 2, 'target': 14, 'value': 2548.6899999999996},
+  {'source': 2, 'target': 15, 'value': 1602.1},
+  {'source': 2, 'target': 16, 'value': 359.66999999999996},
+  {'source': 7, 'target': 17, 'value': 91.61},
+  {'source': 7, 'target': 18, 'value': 183.73},
+  {'source': 7, 'target': 19, 'value': 95.0},
+  {'source': 7, 'target': 20, 'value': 34.24},
+  {'source': 7, 'target': 21, 'value': 285.1},
+  {'source': 7, 'target': 22, 'value': 73.96000000000001},
+  {'source': 3, 'target': 23, 'value': 95.61},
+  {'source': 3, 'target': 24, 'value': 2829.3699999999967},
+  {'source': 3, 'target': 25, 'value': 780.87},
+  {'source': 3, 'target': 26, 'value': 1345.6299999999999},
+  {'source': 4, 'target': 27, 'value': 495.80000000000007},
+  {'source': 4, 'target': 28, 'value': 577.55},
+  {'source': 14, 'target': 29, 'value': 24.0},
+  {'source': 14, 'target': 30, 'value': 395.84000000000003},
+  {'source': 14, 'target': 31, 'value': 6.5},
+  {'source': 14, 'target': 32, 'value': 625.5999999999999},
+  {'source': 14, 'target': 33, 'value': 1496.75},
+  {'source': 15, 'target': 34, 'value': 1602.1},
+  {'source': 8, 'target': 35, 'value': 2223.02},
+  {'source': 8, 'target': 36, 'value': 1593.3500000000006},
+  {'source': 9, 'target': 37, 'value': 284.45000000000005},
+  {'source': 9, 'target': 38, 'value': 714.61},
+  {'source': 9, 'target': 39, 'value': 1037.0800000000002},
+  {'source': 9, 'target': 40, 'value': 2223.75},
+  {'source': 9, 'target': 41, 'value': 190.62},
+  {'source': 9, 'target': 42, 'value': 48.33},
+  {'source': 9, 'target': 43, 'value': 365.19999999999993},
+  {'source': 10, 'target': 44, 'value': 109.36},
+  {'source': 11, 'target': 45, 'value': 82.62},
+  {'source': 11, 'target': 46, 'value': 75.0},
+  {'source': 11, 'target': 47, 'value': 58.33},
+  {'source': 12, 'target': 48, 'value': 2316.61},
+  {'source': 12, 'target': 49, 'value': 159.81},
+  {'source': 12, 'target': 50, 'value': 14.83},
+  {'source': 12, 'target': 51, 'value': 188.23},
+  {'source': 12, 'target': 52, 'value': 119.96},
+  {'source': 12, 'target': 53, 'value': 84.0},
+  {'source': 12, 'target': 54, 'value': 232.0},
+  {'source': 12, 'target': 55, 'value': 40.52},
+  {'source': 5, 'target': 52, 'value': 396.7900000000001},
+  {'source': 5, 'target': 56, 'value': 719.6600000000001},
+  {'source': 5, 'target': 57, 'value': 40.47},
+  {'source': 5, 'target': 58, 'value': 261.41},
+  {'source': 5, 'target': 59, 'value': 10.61},
+  {'source': 5, 'target': 60, 'value': 202.19},
+  {'source': 5, 'target': 61, 'value': 208.0},
+  {'source': 16, 'target': 62, 'value': 15.03},
+  {'source': 16, 'target': 63, 'value': 278.84},
+  {'source': 16, 'target': 64, 'value': 20.0},
+  {'source': 16, 'target': 65, 'value': 45.8},
+  {'source': 6, 'target': 66, 'value': 594.41},
+  {'source': 6, 'target': 67, 'value': 752.6500000000001}]}
+;
+ 
+   var margin = {
+    top : 5,
+    right : 1,
+    bottom : 6,
+    left : 1
+   }, 
+
+   width = 800 - margin.left - margin.right, 
+   height = 900 - margin.top - margin.bottom;
+   var formatNumber = d3.format(",.0f"), format = function(d) {
+    return formatNumber(d) ;
+   }, 
+   color = d3.scale.category20();
+   
+   var svg = d3.select("#chart")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+   var sankey = d3.sankey()
+    .nodeWidth(15)
+    .nodePadding(10)
+    .size([width, height]);
+    
+   var path = sankey.link();
+   
+   sankey.nodes(alistairData.nodes)
+    .links(alistairData.links)
+    .layout(32);
+      
+   var link = svg.append("g")
+      .selectAll(".link")
+      .data(alistairData.links)
+      .enter()
+      .append("path")
+      .attr("class", "link")
+      .attr("d", path)
+      .style("stroke-width", 
+       function(d) {
+        return Math.max(1, d.dy);
+       })
+      .sort(function(a, b) {
+       return b.dy - a.dy;
+       });
+    
+   link.append("title")
+     .text(function(d) {
+      return d.source.name + " ? " + d.target.name + "\n" + format(d.value);
+      });
+      
+   var node = svg.append("g")
+      .selectAll(".node")
+      .data(alistairData.nodes)
+      .enter()
+      .append("g")
+      .attr("class", "node")
+      .attr("transform", 
+       function(d) {
+         return "translate(" + d.x + "," + d.y + ")";
+        }
+       )
+      .call(d3.behavior.drag()
+      .origin(function(d) {
+         return d;
+        }
+       )
+      .on("dragstart", function() {
+        this.parentNode.appendChild(this);
+        }
+       )
+      .on("drag", dragmove));
+      
+      node.append("rect")
+       .attr("height", 
+         function(d) {
+          return d.dy;
+         }
+        )
+       .attr("width", sankey.nodeWidth())
+       .style("fill", function(d) {
+          return d.color = color(d.name.replace(/ .*/, ""));
+         }
+        )
+       .style("stroke", function(d) {
+           return d3.rgb(d.color).darker(2);
+          }
+         )
+       .append("title")
+       .text(function(d) {
+          return d.name + "\n" + format(d.value);
+         }
+        );
+        
+      node.append("text")
+      .attr("x", -6)
+      .attr("y", function(d) {
+          return d.dy / 2;
+         }
+        )
+      .attr("dy", ".35em")
+      .attr("text-anchor", "end")
+      .attr("transform", null)
+      .text(function(d) {
+         return d.name;
+        }
+       )
+      .filter(function(d) {
+         return d.x < width / 2;
+       }
+      )
+      .attr("x", 6 + sankey.nodeWidth())
+      .attr("text-anchor", "start");
+    
+     function dragmove(d) {
+      d3.select(this)
+       .attr("transform", "translate(" + d.x + "," + (d.y = Math.max(0, Math.min(height - d.dy, d3.event.y))) + ")");
+       sankey.relayout();
+      link.attr("d", path);
+     }
